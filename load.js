9dcf7e8a338f7a7cbf82c41ed0935d34617a9749
srishtigulecha <srishtigulecha02@gmail.com>
2023-04-21 11:29:46 +0530

    final one

--- load.js
+++ load.js
@@ -1,6 +1,6 @@
-// $(document).ready(function () {
-//     // Handler for .ready() called.
-//     window.setTimeout(function () {
-//         location.href = "dark.html";
-//     }, 3000);
-// });
+$(document).ready(function () {
+    // Handler for .ready() called.
+    window.setTimeout(function () {
+        location.href = "dark.html";
+    }, 3000);
+});
